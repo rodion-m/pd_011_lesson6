d0487ff42bab15f1f03201186033e16bd9f81ef1
Rodion Mostovoy <36400912+rodion-m@users.noreply.github.com>
2022-05-25 20:53:54 +0300

    Initial commit

---
+++ WebApplication7/Controllers/CatalogController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Concurrent;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication7.Models;
+
+namespace WebApplication7.Controllers;
+
+public class CatalogController : Controller
+{
+    private static ConcurrentBag<Product> _catalog = new();
+    
+    [HttpPost]
+    public IActionResult ProductAdding([FromForm] Product product)
+    {
+        _catalog.Add(product);
+        return View(_catalog);
+    }
+    
+    [HttpGet]
+    public IActionResult ProductAdding()
+    {
+        return View(_catalog);
+    }
+}
